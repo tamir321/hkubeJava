0d2330c09977b3a9afb78e6e71e1b91e75ed74f2
tamir321 <48160658+tamir321@users.noreply.github.com>
2020-06-25 10:08:53 +0300

    first algorithm

---
+++ src/main/java/algorithm.java
@@ -0,0 +1,69 @@
+import hkube.algo.wrapper.IAlgorithm;
+import hkube.api.IHKubeAPI;
+import hkube.api.INode;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Algorithm implements IAlgorithm {
+
+
+    @Override
+    public void Init(JSONObject args) {
+
+    }
+
+    @Override
+    public JSONObject Start(JSONArray input, IHKubeAPI hkubeAPI) throws Exception {
+        Map<String, Object> data = new HashMap<>();
+        data.put("myAnswer", 33);
+        data.put("mirror", input);
+      //  JSONObject result =  hkubeAPI.startAlgorithm("green-alg",new JSONArray(),false);
+        JSONObject simpleInput = new JSONObject();
+        Map files = new HashMap();
+        files.put("link","mylink");
+        simpleInput.put("files",files);
+      //  hkubeAPI.startStoredPipeLine("simple",simpleInput);
+        INode node = new INode() {
+            @Override
+            public String getName() {
+                return "yellow-alg-NOde";
+            }
+
+            @Override
+            public JSONObject[] getInput() {
+                return new JSONObject[0];
+            }
+
+            @Override
+            public void setInput(JSONObject[] input) {
+
+            }
+            @Override
+            public String getAlgorithmName() {
+                return "yellow-alg";
+            }
+
+            @Override
+            public void setAlgorithmName(String algorithmName) {
+
+            }
+        };
+        INode[] nodes ={node};
+      //  hkubeAPI.startRawSubPipeLine("myRow",nodes,new JSONObject(),new HashMap(),new HashMap());
+        //return new JSONObject(result);
+        return simpleInput;
+    }
+
+    @Override
+    public void Stop() {
+
+    }
+
+    @Override
+    public void Cleanup() {
+
+    }
+}
