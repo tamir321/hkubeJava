d791cbf341198b3746bd734de79ce38d66b3662a
tamir reiss <tamir321@gmail.com>
2021-04-18 11:47:49 +0300

    add new java api for Amit

---
+++ src/main/java/D.java
@@ -0,0 +1,57 @@
+import hkube.algo.wrapper.IAlgorithm;
+import hkube.api.IHKubeAPI;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class D implements IAlgorithm {
+
+    public Object Start(Map args, IHKubeAPI hkubeAPI) throws Exception {
+
+
+
+        String nodeName = (String) args.get("nodeName");
+        Collection<Object> input = (Collection<Object>)args.get("input");
+        Iterator iterator = input.iterator();
+        String inputAsString ="";
+        while (iterator.hasNext()) {
+            Object action =  input.iterator().next();
+            if (action instanceof  String){
+                inputAsString = inputAsString + action;
+            }
+            else if(action instanceof List){
+                inputAsString = inputAsString + "[";
+                Iterator iterator1 = ((List)action).iterator();
+                while (iterator1.hasNext()){
+                    action =  input.iterator().next();
+                    inputAsString = inputAsString + action;
+                }
+                inputAsString = inputAsString + "]";
+            }
+        }
+
+       /// String act = (String)action.get("input");
+
+        String result = "I am "+ nodeName +" I recived the following input-" + inputAsString;
+        return result;
+
+    }
+
+    @Override
+    public void Init(Map map) {
+
+    }
+
+
+    @Override
+    public void Stop() {
+
+    }
+
+    @Override
+    public void Cleanup() {
+
+    }
+}
